adf08ed1f1a56cf008e6641d34d9e3f047d3eef6
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-24 02:10:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,27 @@ data.each do |colors_gender_lives, values|
     array.each do |name|
       if hash[name] == nil
         hash [name] = {}
+          hash[name][colors_gender_lives] = []
+      else
+          hash[name][colors_gender_lives] = []
+        end
+      end
+    end
+  end
+hash.each do |name, values|
+values.each do |hashvalue,array|
+data.each do |colors_gender_lives, values|
+  values.each do |value, array|
 
+    array.each do |element|
+      if element == name && hashvalue == colors_gender_lives
 
-
+        hash[name] << value.to_s
+      end
+    end
+  end
+end
+end
+end
+hash
 end
